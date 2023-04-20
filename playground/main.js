85eb02615b1b52fec2b32b2f0182b003372d1ee9
ChanIok <chaniok@qq.com>
2023-04-20 17:38:32 +0800

    feat: 获取最新的manifest

--- playground/main.js
+++ playground/main.js
@@ -1,5 +1,5 @@
 import { Uploader } from "./Uploader.js";
-
+import { getLatestManifestId, getLatestState } from "./utils.js";
 const config = {
   distPath: "../dist",
   walletPath: "../wallet.json",
@@ -9,4 +9,8 @@ const args = process.argv.slice(2);
 const uploader = new Uploader(config.distPath, config.walletPath);
 if (args[0] == "upload") {
   uploader.uploadMissingFiles();
+} else if (args[0] == "getId") {
+  console.log(await getLatestManifestId());
+} else if (args[0] == "getMF") {
+  console.log(await getLatestState(args[1]));
 }
