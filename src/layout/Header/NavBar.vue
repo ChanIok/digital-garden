c46e839fdeeb23b56fc8e568d34baa484737ff65
ChanIok <chaniok@qq.com>
2023-04-27 17:41:53 +0800

    feat: 面包屑省略

--- src/layout/Header/NavBar.vue
+++ src/layout/Header/NavBar.vue
@@ -1,26 +1,67 @@
 <template>
-  <n-breadcrumb>
-    <n-breadcrumb-item
-      v-for="(item, index) in writingStore.currentWritingPathArray"
-      :key="index"
-      @click="goToPath(item, index)"
-    >
+  <n-breadcrumb id="nav-bar" ref="nav-bar">
+    <n-breadcrumb-item>
+      <router-link to="/">首页</router-link>
+    </n-breadcrumb-item>
+
+    <n-breadcrumb-item v-if="hiddenList.length">
+      <n-dropdown :options="hiddenList" @select="handleSelect">
+        <div class="trigger">
+          ...
+        </div>
+      </n-dropdown>
+    </n-breadcrumb-item>
+
+    <n-breadcrumb-item v-for="(item, index) in visibleList" :key="index" @click="goToPath(item, index)">
       {{ item.replace(/%20/g, " ") }}
     </n-breadcrumb-item>
+
   </n-breadcrumb>
 </template>
 
 <script setup lang="ts">
-import { NBreadcrumb, NBreadcrumbItem } from "naive-ui";
+import { NBreadcrumb, NBreadcrumbItem, NDropdown } from "naive-ui";
 import { useWritingStore } from "@/store";
 import { useRouter } from "vue-router";
+import { computed, nextTick, onMounted, ref, unref } from "vue";
+import { watch } from "fs";
 const router = useRouter();
 const writingStore = useWritingStore();
+const navBar = ref<any>(null);
+const visibleList = ref<any>([]);
+const hiddenList = ref<any>([]);
+
+
+
+const loadBreadcrumbData = async () => {
+  visibleList.value = writingStore.currentWritingPathArray.slice()
+  hiddenList.value = []
+  await nextTick()
+  const navBarUl = document.querySelector('#nav-bar>ul');
+  while ((navBarUl as any).offsetWidth > window.innerWidth - 100) {
+    const item = visibleList.value.shift()
+    hiddenList.value.push({
+      label: item,
+      key: item
+    });
+    await nextTick()
+  }
+}
+onMounted(async () => {
+  loadBreadcrumbData();
+})
+
+writingStore.$subscribe((mutation, state) => {
+  if ((mutation.events as any).key == "currentWritingPath") {
+    loadBreadcrumbData();
+  }
+});
 
 const goToPath = (pathName: string, clickIndex: number) => {
   if (clickIndex == writingStore.currentWritingPathArray.length - 1) {
     return;
   }
+  console.log(pathName)
   const pathArray = writingStore.currentWritingPathArray;
   const index = pathArray.indexOf(pathName);
   if (index != -1) {
@@ -29,6 +70,31 @@ const goToPath = (pathName: string, clickIndex: number) => {
     router.push(`/${temp.join("/")}`);
   }
 };
+const handleSelect = (key: string) => {
+  const pathArray = writingStore.currentWritingPathArray;
+  const index = pathArray.indexOf(key);
+  console.log(key, pathArray)
+  if (index != -1) {
+    const temp = pathArray.slice(0, index + 1);
+    temp.push("index.md");
+    router.push(`/${temp.join("/")}`);
+  }
+}
+
 </script>
 
-<style lang="less" module></style>
+<style lang="less" scoped>
+#nav-bar {
+  max-width: 100%;
+
+  :deep(>ul) {
+    display: flex;
+  }
+
+  .trigger {
+    padding: 4px;
+    margin: -4px;
+    border-radius: inherit;
+  }
+}
+</style>
