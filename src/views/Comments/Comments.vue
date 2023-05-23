27695a2cf08f094406978867588e0efe46b5981a
ChanIok <chaniok@qq.com>
2023-05-23 16:57:45 +0800

    chore: 留言板去除说明

--- src/views/Comments/Comments.vue
+++ src/views/Comments/Comments.vue
@@ -1,20 +1,14 @@
 <template>
     <div id="comments">
         <div class="container">
-            <div class="header">
-                <n-el tag="span"> 这是一块去中心化的留言板，你可以在这里写点东西... </n-el>
-                <n-divider />
-            </div>
-
             <div class="content">
                 <iframe :src="url" frameborder="0"></iframe>
             </div>
         </div>
     </div>
 </template>
-  
+
 <script setup lang="ts">
-import { NDivider, NEl } from "naive-ui";
 import { useStore } from "@/store";
 import { computed } from "vue";
 
@@ -38,7 +32,6 @@ const url = computed(() => {
     .container {
         width: 100%;
         height: 100%;
-
         display: flex;
         flex-direction: column;
 
