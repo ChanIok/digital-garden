59cf60f9901224404f8f92366a7e037a323f5506
ChanIok <chaniok@qq.com>
2023-08-26 19:13:50 +0800

    chore: 跳转链接优化

--- src/views/Status/Status.vue
+++ src/views/Status/Status.vue
@@ -1,54 +1,47 @@
 <template>
-  <div id="status">
-    <div class="container">
-      <div class="content">
-        <n-h1>区块状态</n-h1>
-        <n-h2>文章入口</n-h2>
-        <n-timeline>
-          <n-timeline-item
-            v-for="(item, index) in writingsEdges"
-            :type="index == 0 ? 'success' : 'info'"
-            :key="index"
-            :title="`${item.node.id}`"
-            :content="`Height: ${item.node.block.height}`"
-            :time="dayjs.unix(item.node.block.timestamp).format('YYYY-MM-DD HH:mm:ss')"
-            :line-type="index == writingsEdges.length - 1 ? 'dashed' : 'default'"
-            @click="onClick(item.node.id)"
-          />
-          <n-timeline-item content="......" />
-        </n-timeline>
+  <div id="status" ref="status">
+    <n-scrollbar>
+      <div class="container">
 
-        <n-h2>站点入口</n-h2>
-        <n-timeline>
-          <n-timeline-item
-            v-for="(item, index) in AppEdges"
-            :type="index == 0 ? 'success' : 'info'"
-            :key="index"
-            :title="`${item.node.id}`"
-            :content="`Height: ${item.node.block.height}`"
-            :time="dayjs.unix(item.node.block.timestamp).format('YYYY-MM-DD HH:mm:ss')"
-            :line-type="index == AppEdges.length - 1 ? 'dashed' : 'default'"
-            @click="onClick(item.node.id)"
-          />
-          <n-timeline-item content="......" />
-        </n-timeline>
+        <div class="content">
+          <n-h1>区块状态</n-h1>
+          <n-h2>文章入口</n-h2>
+          <n-timeline>
+            <n-timeline-item v-for="(item, index) in writingsEdges" :type="index == 0 ? 'success' : 'info'" :key="index"
+              :title="`${item.node.id}`" :content="`Height: ${item.node.block.height}`"
+              :time="dayjs.unix(item.node.block.timestamp).format('YYYY-MM-DD HH:mm:ss')"
+              :line-type="index == writingsEdges.length - 1 ? 'dashed' : 'default'" />
+            <n-timeline-item content="......" />
+          </n-timeline>
+
+          <n-h2>站点入口</n-h2>
+          <n-timeline>
+            <n-timeline-item v-for="(item, index) in AppEdges" :type="index == 0 ? 'success' : 'info'" :key="index"
+              :title="`${item.node.id}`" :content="`Height: ${item.node.block.height}`"
+              :time="dayjs.unix(item.node.block.timestamp).format('YYYY-MM-DD HH:mm:ss')"
+              :line-type="index == AppEdges.length - 1 ? 'dashed' : 'default'" />
+            <n-timeline-item content="......" />
+          </n-timeline>
+        </div>
+
       </div>
-    </div>
+    </n-scrollbar>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { NTimeline, NTimelineItem, NH1, NH2 } from 'naive-ui';
-  import { getLatestManifest } from '@/utils/artools';
-  import { nextTick, onMounted, ref } from 'vue';
-  import dayjs from 'dayjs';
+import { NTimeline, NTimelineItem, NH1, NH2, NScrollbar } from 'naive-ui';
+import { getLatestManifest } from '@/utils/artools';
+import { nextTick, onMounted, ref, h, render } from 'vue';
+import dayjs from 'dayjs';
+const status = ref<any>(null);
 
-  const writingsEdges = ref<any>(null);
-  const AppEdges = ref<any>(null);
-  const onClick = (txId: string) => {
-    window.open(`https://viewblock.io/arweave/tx/${txId}`, '_blank');
-  };
-  const a = async () => {
+
+
+const writingsEdges = ref<any>(null);
+const AppEdges = ref<any>(null);
+
+const a = async () => {
   const [writingsResponse, appResponse] = await Promise.all([
     getLatestManifest(),
     getLatestManifest(true)
@@ -58,41 +51,64 @@
   AppEdges.value = appResponse.data.transactions.edges;
 };
 
-  onMounted(() => a());
+const b = () => {
+  const elements = Array.from(status.value.querySelectorAll('.n-timeline-item-content__title'));
+  elements.map((item: any) => {
+    const txId = item.innerHTML;
+    const aInstance = h(
+      'a',
+      {
+        href: `https://viewblock.io/arweave/tx/${txId}`,
+        target: "_blank"
+      }, txId
+    );
+
+    let aElement = document.createElement('div');
+    render(aInstance, aElement);
+    item.replaceWith(aElement);
+
+  })
+};
+
+onMounted(async () => {
+  await a(),
+    b()
+});
 </script>
 
 <style lang="less" scoped>
-  #status {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    justify-content: center;
+#status {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+
+
+  .container {
     align-items: center;
+    justify-content: center;
     display: flex;
 
-    .container {
-      padding: 20px;
-      box-sizing: border-box;
-      max-width: 880px;
-      height: 100%;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      //   display: flex;
-      //   flex-direction: column;
+    width: 100%;
 
-      .content {
-        width: 880px;
+    .content {
+      padding: 40px;
+      box-sizing: border-box;
+      width: 880px;
+      max-width: 100%;
+
+      :deep(a) {
+        text-decoration: none;
+        color: inherit;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
         max-width: 100%;
-        :deep(.n-timeline-item-content__title) {
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          max-width: 100%;
-          overflow: hidden;
-        }
-        :deep(.n-timeline-item-content) {
-          cursor: pointer;
-        }
+        display: inline-block;
+        font-size: 14px;
       }
+
     }
   }
+}
 </style>
