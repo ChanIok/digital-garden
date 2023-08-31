d28fb55b59936bc28f70bbc5c07adc6488cc6f87
ChanIok <chaniok@qq.com>
2023-09-01 05:02:17 +0800

    chore: 调整样式

--- src/layout/Header/Header.vue
+++ src/layout/Header/Header.vue
@@ -9,19 +9,19 @@
 </template>
 
 <script setup lang="ts">
-import NavBar from "@/layout/Header/NavBar.vue";
-import Options from "@/layout/Header/Options.vue";
-import LoadingBar from "@/layout/Header/LoadingBar.vue";
-import { NLoadingBarProvider } from "naive-ui";
+  import NavBar from '@/layout/Header/NavBar.vue';
+  import Options from '@/layout/Header/Options.vue';
+  import LoadingBar from '@/layout/Header/LoadingBar.vue';
+  import { NLoadingBarProvider } from 'naive-ui';
 </script>
 
 <style lang="less">
-#header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-sizing: border-box;
-  padding: 0 10px 0 10px;
-  height: 40px;
-}
+  #header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 0 10px 0 10px;
+    height: 40px;
+  }
 </style>
